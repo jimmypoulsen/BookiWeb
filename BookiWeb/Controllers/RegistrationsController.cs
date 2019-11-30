9d5cc62861d7cde379fd6f8f08e6c0a0888349e3
Jimmy Poulsen <jimmy@progras.dk>
2019-11-30 02:18:14 +0100

    Add RegistrationsController to handle customer registrations

---
+++ BookiWeb/Controllers/RegistrationsController.cs
@@ -0,0 +1,45 @@
+﻿using BookiWeb.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookiWeb.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        string BaseUrl = "https://localhost:44314/api/";
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            var customerInfo = new
+            {
+                Customer = customer
+            };
+            var json = JsonConvert.SerializeObject(customerInfo);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = BaseUrl + "/customers";
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, data);
+                string result = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction("Create", "Sessions");
+                else
+                    return RedirectToAction("Create");
+            }
+        }
+    }
+}
